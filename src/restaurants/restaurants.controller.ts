d7a92a124cc021ef8e0d872326e3c8cef15a5405
Tâm Phạm <tamphamasd@gmail.com>
2023-08-05 16:58:52 +0700

    first commit

---
+++ src/restaurants/restaurants.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { RestaurantsService } from './restaurants.service';
+import { Restaurant } from './schemas/restaurant.schema';
+
+@Controller('restaurants')
+export class RestaurantsController {
+  constructor(private restaurantsService: RestaurantsService) {}
+
+  @Get()
+  async getAllRestaurants(): Promise<Restaurant[]> {
+    return this.restaurantsService.findAll();
+  }
+
+  @Post()
+  async createRestaurant(
+    @Body()
+    restaurant,
+  ): Promise<Restaurant> {
+    return this.restaurantsService.create(restaurant);
+  }
+}
---
+++ src/restaurants/restaurants.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Restaurant } from './schemas/restaurant.schema';
+
+@Injectable()
+export class RestaurantsService {
+  constructor(
+    @InjectModel(Restaurant.name)
+    private restaurantModel: mongoose.Model<Restaurant>,
+  ) {}
+
+  // Get all Reataurants => GET /restaurants
+  async findAll(): Promise<Restaurant[]> {
+    const restaurants = await this.restaurantModel.find();
+    return restaurants;
+  }
+
+  // Create a Restaurant => POST /restaurants
+  async create(restaurant: Restaurant): Promise<Restaurant> {
+    const res = await this.restaurantModel.create(restaurant);
+    return res;
+  }
+}
